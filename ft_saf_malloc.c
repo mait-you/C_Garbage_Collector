be518d18b31bd035461a2543e30ea437aa48de70
hamzahat <hamzabenmohamde2005@gmail.com>
2025-03-02 08:46:38 +0000

    fix bug

--- ft_saf_malloc.c
+++ ft_saf_malloc.c
@@ -6,7 +6,7 @@
 /*   By: hamza_hat <hamza_hat@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 22:14:02 by hamza_hat         #+#    #+#             */
-/*   Updated: 2025/03/01 20:15:08 by hamza_hat        ###   ########.fr       */
+/*   Updated: 2025/03/02 08:44:50 by hamza_hat        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 #include <stdlib.h>
 
 // ft_saf_malloc struct:
-typedef struct s_mem_node
+typedef struct	s_mem_node
 {
-	void *address;
-	struct s_mem_node *next;
-} t_mem_node;
+	void				*address;
+	struct s_mem_node	*next;
+}				t_mem_node;
 
 // free all allocated memory:
 void	free_list(t_mem_node **list, int exit_status)
 {
-	t_mem_node *tmp;
+	t_mem_node	*tmp;
 
 	while (*list)
 	{
@@ -37,20 +37,20 @@ void	free_list(t_mem_node **list, int exit_status)
 
 void	lst_add_back(t_mem_node **lst, void *value)
 {
-	t_mem_node *last;
-	t_mem_node *tmp;
+	t_mem_node	*last;
+	t_mem_node	*tmp;
 
 	if (!lst || !value)
 		return ;
 	tmp = malloc(sizeof(t_mem_node));
-	if(!tmp)
+	if (!tmp)
 		free_list(lst, 1);
 	tmp->address = value;
 	tmp->next = NULL;
 	if (!*lst)
 	{
 		*lst = tmp;
-		return;
+		return ;
 	}
 	last = *lst;
 	while (last->next)
@@ -60,19 +60,19 @@ void	lst_add_back(t_mem_node **lst, void *value)
 
 void	*ft_malloc(size_t size, int key, int exit_status)
 {
-	// Static pointer to the head of the list
-	static t_mem_node *mem_node = NULL;
-	void	*ptr;
+	// Static pointer to the head of the list:
+	static t_mem_node	*mem_node;
+	void				*ptr;
 
 	ptr = NULL;
-	// Key == 1 → Allocate memory
-	if (key == 1)
+	if (key == 1) 	// Key = 1 → Allocate memory
 	{
-		if (!(ptr = malloc(size)))
+		ptr = malloc(size);
+		if (!ptr)
 			free_list(&mem_node, 1);
 		lst_add_back(&mem_node, ptr);
 	}
-	if (key == 0)
+	if (key == 0)	// key = 0 → deallocate memory
 		free_list(&mem_node, exit_status);
 	return (ptr);
 }
